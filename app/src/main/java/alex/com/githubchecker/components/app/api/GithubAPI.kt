d803394272582c73f4170c811d1e4f9b55edd029
Alex Doub <alexdoub1990@gmail.com>
2019-02-11 21:44:47 -0800

    uncomment

--- app/src/main/java/alex/com/githubchecker/components/app/api/GithubAPI.kt
+++ app/src/main/java/alex/com/githubchecker/components/app/api/GithubAPI.kt
@@ -1,5 +1,6 @@
 package alex.com.githubchecker.components.app.api
 
+import alex.com.githubchecker.models.Diff
 import alex.com.githubchecker.models.api.PullRequest
 import io.reactivex.Observable
 import retrofit2.http.GET
@@ -14,10 +15,9 @@ interface GithubAPI {
     @GET("repos/{owner}/{repo}/pulls")
     fun getPullRequests(@Path("owner") owner: String, @Path("repo") repo: String): Observable<List<PullRequest>>
 
-    //    @GET("repos/{owner}/{repo}/pulls/{pr_id}")
-    //    Observable<PullRequest> getPullRequest(@Path("owner") String owner, @Path("repo") String repo, @Path("pr_id") String pullrequestId);
+    @GET("repos/{owner}/{repo}/pulls/{pr_id}")
+    fun getPullRequest(@Path("owner") owner: String, @Path("repo") repo: String, @Path("pr_id") pullrequestId: String): Observable<PullRequest>
 
-    //    @GET("repos/{owner}/{repo}/pull/{pr_id}.diff")
-    //    Observable<Diff> getDiff(@Path("owner") String owner, @Path("repo") String repo, @Path("pr_id") String pullrequestId);
-
+    @GET("repos/{owner}/{repo}/pull/{pr_id}.diff")
+    fun getDiff(@Path("owner") owner: String, @Path("repo") repo: String, @Path("pr_id") pullrequestId: String): Observable<Diff>
 }
--- app/src/main/java/alex/com/githubchecker/components/app/dagger/AppComponent.kt
+++ app/src/main/java/alex/com/githubchecker/components/app/dagger/AppComponent.kt
@@ -4,9 +4,6 @@ import alex.com.githubchecker.components.app.api.APIClient
 import alex.com.githubchecker.components.app.data.DataManager
 import android.content.Context
 import dagger.Component
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
-import javax.inject.Scope
 
 /**
  * Created by Alex on 11/12/2017.
