03a11a36758d50f73f2ff6108e2cc4fb4209c615
Alex Doub <alexdoub1990@gmail.com>
2019-10-30 09:34:01 -0700

    more reorg

--- app/src/main/java/alex/com/githubchecker/GithubCheckerApp.kt
+++ app/src/main/java/alex/com/githubchecker/GithubCheckerApp.kt
@@ -2,12 +2,12 @@ package alex.com.githubchecker
 
 import alex.com.githubchecker.components.app.AppComponent
 import alex.com.githubchecker.components.app.AppModule
+import alex.com.githubchecker.components.app.DaggerAppComponent
 import alex.com.githubchecker.components.app.api.NetworkModule
-import alex.com.githubchecker.components.app.dagger.DaggerAppComponent
 import alex.com.githubchecker.components.app.data.DataModule
+import alex.com.githubchecker.components.github.DaggerGithubComponent
 import alex.com.githubchecker.components.github.GithubComponent
 import alex.com.githubchecker.components.github.GithubModule
-import alex.com.githubchecker.components.github.dagger.DaggerGithubComponent
 import android.app.Application
 import com.facebook.stetho.Stetho
 import timber.log.Timber
@@ -17,6 +17,17 @@ import timber.log.Timber
  */
 
 class GithubCheckerApp : Application() {
+
+    companion object {
+
+        lateinit var appComponent: AppComponent
+
+        val githubComponent: GithubComponent by lazy {
+            DaggerGithubComponent.builder()
+                    .appComponent(appComponent)
+                    .githubModule(GithubModule()).build()
+        }
+    }
 
     override fun onCreate() {
         super.onCreate()
@@ -28,16 +39,5 @@ class GithubCheckerApp : Application() {
                 .appModule(AppModule(this))
                 .networkModule(NetworkModule())
                 .dataModule(DataModule()).build()
-    }
-
-    companion object {
-
-        lateinit var appComponent: AppComponent
-
-        val githubComponent: GithubComponent by lazy {
-            DaggerGithubComponent.builder()
-                    .appComponent(appComponent)
-                    .githubModule(GithubModule()).build()
-        }
     }
 }
